9c80c093566f00e20d1cb5bf006b4aecc009db18
NileshSurve123 <nileshsurve521@gmail.com>
2024-03-24 23:41:51 +0530

    second commit

--- app/User_list/[id]/page.jsx
+++ app/User_list/[id]/page.jsx
@@ -1,42 +1,57 @@
+export async function generateStaticParams() {
+  const res = await fetch("https://602e7c2c4410730017c50b9d.mockapi.io/users")
+  const data = await res.json()
 
-
-
-"use client"
-import React from 'react';
-import {useEffect, useState } from 'react';
-
-
- function UserComponent() {
-  const [userId, setUserId] = useState();
-
-  useEffect(() => {
-    const fetchUserId = async () => {
-      try {
-        const response = await fetch("https://602e7c2c4410730017c50b9d.mockapi.io/users");
-        const data = await response.json();
-        setUserId(data.id);
-      } catch (error) {
-        console.error('Error fetching user ID:', error);
-      }
-    };
-       
-    fetchUserId();
-  }, []);
-
-  return (
-    <div>
-        <p>Loading user ID...</p>
-        <h2>Your Id :- 30</h2>
-        <h1>Username:- Amelia_Hintz{userId?.profile?.username}</h1>
-        <p>firstName: Crystel</p>
-        <p>lastName: Brakus</p>
-        <p>email: Kale_Schneider42@gmail.com</p>
-
-        
-        <p>Job_Tittle:-Chief Accountability Manager {userId?.Bio}</p>
-    
-    </div>
-  );
+  return data.map((user) =>({
+    id: user.id.toString(),
+  }))
 }
 
-export default UserComponent;
+async function getUser(id){
+  const res = await fetch(`https://602e7c2c4410730017c50b9d.mockapi.io/users/${id}`)
+  const data = await res.json();
+  return data
+}
+
+export default async function UserPage({params}){
+  const user = await getUser(params.id);
+  
+  return (
+    <>
+    
+    <img
+        src={user.avatar} width={120} height={120}
+                       />
+                       <br></br>
+
+<h1>your id :- {user.id}</h1>                 
+        <br></br>               
+    Username:-{user.profile.username};
+    <br></br>
+    First_name:- {user.profile.firstName};
+    <br></br>
+    Last_name :- {user.profile.lastName};
+    <br></br>
+
+    Email:- {user.profile.email};   
+                            
+        <br></br>
+      Job_Tittle:- {user.jobTitle};
+      <br></br>
+      Bio:- {user.Bio}
+
+
+    </>
+  )
+  
+};
+  
+
+
+
+   
+
+
+
+
+
